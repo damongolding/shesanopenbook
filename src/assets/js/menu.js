95e263d261effdbd25d1a25e9d6119dd57be6b39
Damon <damon@damongolding.com>
2023-10-16 11:37:31 +0100

    Together we can do so much FIX

--- src/assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menu-button");
-const header = document.getElementsByTagName("header")[0];
-
-menuButton.addEventListener("click", () => {
-    header.classList.toggle("open");
-});
